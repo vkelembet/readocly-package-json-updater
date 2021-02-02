15ac6d58686282a2cf0bd8c20d029cf70453c8d4
Volodymyr Kelembet <vova.kelembet@gmail.com>
2021-02-02 13:23:18 +0200

    Load files as ES modules. Paser script input params

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log('Hello Readocly!')
 
+import minimist from 'minimist'
+const argv = minimist(process.argv.slice(2))
+console.log(argv)
+
 // TODO: accept params using something like minimist or yargs: package name, version, repository to update
 // TODO: use https://www.npmjs.com/package/bitbucket to auth to provided repo
 // TODO: update package.json with new package version
