f4a020714b28c4a50b0e874f5cfe0c706830321d
Volodymyr Kelembet <vova.kelembet@gmail.com>
2021-02-02 13:17:56 +0200

    Figure out what to do :)

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('Hello Readocly!')
+
+// TODO: accept params using something like minimist or yargs: package name, version, repository to update
+// TODO: use https://www.npmjs.com/package/bitbucket to auth to provided repo
+// TODO: update package.json with new package version
+// TODO: create PR using bitbucket npm
